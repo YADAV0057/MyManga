c7f8454a3624336eb65918c41196814a38eb5fb8
YADAV0057 <yadavakhil0057@gmail.com>
2026-07-05 19:13:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const db = getFirestore(app); 
 const analytics = getAnalytics(app);
 
 function generateCacheKey(query, page) {
