72664865576b6adbd07af02abec98e6cd66a6f60
YADAV0057 <yadavakhil0057@gmail.com>
2026-07-05 22:52:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCrZAQbMT35SKArRfWnKGt4SS5NlJgN1XM",
   authDomain: "moodmanga-80a58.firebaseapp.com",
   projectId: "moodmanga-80a58",
-  storageBucket: "moodmanga-80a58.firebasestorage.app",
+  storageBucket: "moodmanga-80a58.firebasestorage.app", 
   messagingSenderId: "970051387669",
   appId: "1:970051387669:web:f9789bb0b568eb803ca91c",
   measurementId: "G-JZSZ0TYYEL"
