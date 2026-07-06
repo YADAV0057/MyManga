b27e24df44762959602baed9eed0161884dfd003
YADAV0057 <yadavakhil0057@gmail.com>
2026-07-07 03:11:18 +0530

    Update genreMapper.js

--- js/parser/genreMapper.js
+++ js/parser/genreMapper.js
@@ -5,22 +5,32 @@
  */
 export const MOOD_TO_GENRE_MAP = {
     // EMOTIONS
-    sad: { Drama: 1.0, Tragedy: 0.9, SliceOfLife: 0.4 },
-    emotional: { Drama: 0.8, SliceOfLife: 0.6, Romance: 0.5 },
+    sad: { Drama: 1.0, Tragedy: 0.9 }, 
+    emotional: { Drama: 0.8, Romance: 0.5, Psychological: 0.4 },
     happy: { Comedy: 1.0, SliceOfLife: 0.8, Adventure: 0.5 },
     uplifting: { SliceOfLife: 0.9, Fantasy: 0.6, Comedy: 0.5 },
     
-    // VIBES
+    // VIBES & THEMES
     wholesome: { SliceOfLife: 1.0, Romance: 0.8, Comedy: 0.6 },
     soft: { SliceOfLife: 0.9, Romance: 0.7 },
     dark: { Psychological: 1.0, Horror: 0.9, Thriller: 0.8, Mystery: 0.5 },
     serious: { Drama: 0.8, Psychological: 0.7, Action: 0.5 },
-    
-    // THEMES
     revenge: { Drama: 1.0, Psychological: 0.9, Action: 0.8, Mystery: 0.4 },
     psychological: { Psychological: 1.0, Mystery: 0.8, Thriller: 0.8 },
     romance: { Romance: 1.0, Drama: 0.6, SliceOfLife: 0.5 },
-    comedy: { Comedy: 1.0, SliceOfLife: 0.7, Parody: 0.6 }
+    comedy: { Comedy: 1.0, SliceOfLife: 0.7, Parody: 0.6 },
+
+    // NEW MOODS FROM EXPANDED DICTIONARY
+    tragedy: { Tragedy: 1.0, Drama: 0.9, Psychological: 0.6 },
+    horror: { Horror: 1.0, Psychological: 0.9, Thriller: 0.8, Supernatural: 0.5 },
+    action: { Action: 1.0, Adventure: 0.7, Fantasy: 0.5 },
+    epic: { Action: 1.0, Adventure: 1.0, Fantasy: 0.8 },
+    cool: { Action: 0.8, SciFi: 0.6, Thriller: 0.5 },
+    sports: { Sports: 1.0, Drama: 0.6, Action: 0.5 },
+    parody: { Comedy: 1.0, Parody: 1.0 },
+    mystery: { Mystery: 1.0, Psychological: 0.8, Thriller: 0.8 },
+    thriller: { Thriller: 1.0, Mystery: 0.9, Psychological: 0.9 },
+    mature: { Mature: 1.0, Romance: 0.6, Drama: 0.6 }
 };
 
 /**
@@ -61,4 +71,3 @@ export function mapMoodsToGenres(detectedMoods, maxResults = 3) {
     // 3. Return the top N genres
     return sortedGenres.slice(0, maxResults);
 }
-
