1244aed7e4e65a9b81f13b7fa5984b26056288a1
YADAV0057 <yadavakhil0057@gmail.com>
2026-07-07 01:43:39 +0530

    Update dictionary.js

--- js/parser/dictionary.js
+++ js/parser/dictionary.js
@@ -1,1 +1,44 @@
 
+export const MOOD_DICTIONARY = {
+
+    // EMOTIONS
+    "cry": {
+        moods: ["sad", "emotional"],
+        intensity: 0.9
+    },
+
+    "crying": {
+        moods: ["sad", "emotional"],
+        intensity: 0.9
+    },
+
+    "happy": {
+        moods: ["happy", "uplifting"],
+        intensity: 0.6
+    },
+
+    "wholesome": {
+        moods: ["wholesome", "soft"],
+        intensity: 0.5
+    },
+
+    "dark": {
+        moods: ["dark", "serious"],
+        intensity: 0.8
+    },
+
+    "revenge": {
+        moods: ["dark", "revenge", "psychological"],
+        intensity: 0.9
+    },
+
+    "romance": {
+        moods: ["romance"],
+        intensity: 0.6
+    },
+
+    "funny": {
+        moods: ["comedy"],
+        intensity: 0.4
+    }
+};
