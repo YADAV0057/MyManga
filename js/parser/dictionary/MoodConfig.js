ceb056fa4db1592f055c2a17e3b6d94cfaf76c12
YADAV0057 <yadavakhil0057@gmail.com>
2026-07-07 19:28:18 +0530

    Update MoodConfig.js

--- js/parser/dictionary/MoodConfig.js
+++ js/parser/dictionary/MoodConfig.js
@@ -1,13 +1,12 @@
+export const SOURCE_MULTIPLIERS = { Genre: 1.0, Theme: 0.8, Demographic: 0.2 };
 
-module.exports = {
-    SOURCE_MULTIPLIERS: { Genre: 1.0, Theme: 0.8, Demographic: 0.2 },
-    GENRE_WEIGHTS: {
-        "Action": { exciting: 1.0, violent: 0.8 },
-        "Psychological": { dark: 0.9, mysterious: 0.7, emotional: 0.5 },
-        "Drama": { emotional: 0.9, tragic: 0.6 }
-    }, 
-    THEME_WEIGHTS: {
-        "Revenge": { dark: 0.8, emotional: 0.5, violent: 0.7 },
-        "Betrayal": { dark: 0.9, emotional: 0.8 }
-    }
+export const GENRE_WEIGHTS = {
+    "Action": { exciting: 1.0, violent: 0.8 },
+    "Psychological": { dark: 0.9, mysterious: 0.7, emotional: 0.5 },
+    "Drama": { emotional: 0.9, tragic: 0.6 }
+}; 
+
+export const THEME_WEIGHTS = {
+    "Revenge": { dark: 0.8, emotional: 0.5, violent: 0.7 },
+    "Betrayal": { dark: 0.9, emotional: 0.8 }
 };
