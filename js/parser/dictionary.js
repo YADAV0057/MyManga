a14eb4321ba8c8bda27654b77bdf4df187a97317
YADAV0057 <yadavakhil0057@gmail.com>
2026-07-07 23:34:25 +0530

    Update dictionary.js

--- js/parser/dictionary.js
+++ js/parser/dictionary.js
@@ -7,8 +7,8 @@ import { CONCEPT_PROPERTIES as BASE_PROPERTIES } from './dictionary/properties.j
 // We use a safe check; if the file doesn't exist yet, we default to an empty object
 let HARVESTED_RULES = {};
 try {
-    const { HARVESTED_RULES } = await import('./dictionary/harvested_knowledge.js');
-    HARVESTED_RULES = data.HARVESTED_RULES;
+    const harvestedModule = await import('./dictionary/harvested_knowledge.js');
+    HARVESTED_RULES = harvestedModule.HARVESTED_RULES || {};
 } catch (e) {
     console.warn("[System] Harvested knowledge not found, using base only.");
 }
